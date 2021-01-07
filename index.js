b0feacae3801d1bedd8f2ffc842ae16c9e5da977
prtk07 <prateekverma70@gmail.com>
2021-01-07 20:35:27 +0530

    node connect with pg localhost

--- index.js
+++ index.js
@@ -2,26 +2,46 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'postgres',
+    password: 'prtk555',
+    host: 'localhost',
+    database: 'postgres'
+});
+
+
+
+
 
 const app = express()
-    .use(bodyParser.json())
     .use(cors())
+    .use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    // console.log(req.body);
-    fs.appendFile('./users', 'htmldfsdfs', (err) => {
-        if (err) console.log(err);
-        else console.log('saved');
-    });
+app.get("/", (req, res) => {
+    console.log(req);
 })
 
-app.post('/login', (req, res) => {
-    console.log(req.body);
-    let x = JSON.stringify(req.body)
-    fs.appendFile('./users', x, (err) => {
-        if (err) console.log(err);
-        else console.log('saved');
-    })
-});
+let obj = {
+    message: "received"
+}
+
+app.post("/signup", async (req, res) => {
+    try {
+        
+        await client.connect()
+        console.log("sucessfully connected")
+        await client.query(`insert into users2 values ('${req.body.user}','${req.body.password}')`)
+        let result = await client.query('select * from users2;')
+        console.log(result.rows)
+    }
+    catch (err) {
+        console.log(err);
+    }
+    finally {
+        await client.end();
+    }
+})
 
 app.listen(8080);
